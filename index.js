b58ee65feccad35abbb15515c5ccbd8fa80901c7
RosaliaCampusano <rosalia.campusano@ikasle.aeg.eus>
2025-09-09 08:40:05 +0200

    feat: make the exercise two dividing by functions

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ donutWithHigherProtein();
 donutWithLessFibre();
 donutWithHigherCalorie();
 
+console.log("--------- EJERCICIO 2 -------");
+listDonutsWithCalories();
+listDonutWithCarbohydrate();
+averageOfCalories();
+sumOfAllSaturatedFat();
+averagePercentOfEachVitamin();
+
 function donutWithHigherSugar() {
   let higherSugar = donut[0];
   for (let i = 0; i < donut.length; i++) {
@@ -91,3 +98,131 @@ function donutWithHigherCalorie() {
 
   console.log(`El donut con mas calorias es ${higherCalorie.name}`);
 }
+
+function listDonutsWithCalories() {
+  for (let i = 0; i < donut.length; i++) {
+    console.log(
+      `${i + 1} . Name: ${donut[i].name}, Calories: ${
+        donut[i].nutrition_facts.nutrition.calories
+      }`
+    );
+  }
+  console.log();
+}
+
+function listDonutWithCarbohydrate() {
+  for (let i = 0; i < donut.length; i++) {
+    console.log(
+      `${i + 1}. Name: ${donut[i].name}, Carbohydrate: ${
+        donut[i].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre
+      } de fibra y ${
+        donut[i].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars
+      } de azucar`
+    );
+  }
+  console.log();
+}
+
+function averageOfCalories() {
+  let sumOfCalories = 0;
+
+  for (let i = 0; i < donut.length; i++) {
+    sumOfCalories += donut[i].nutrition_facts.nutrition.calories;
+  }
+
+  let averageCalories = sumOfCalories / donut.length;
+
+  console.log(`La media de las calorias de los donut es de ${averageCalories}`);
+}
+
+function sumOfAllSaturatedFat() {
+  let totalSumOfSaturedFat = 0;
+
+  for (let i = 0; i < donut.length; i++) {
+    const saturedFat =
+      +donut[i].nutrition_facts.nutrition.fat.fat_type.saturated.split("g")[0];
+
+    totalSumOfSaturedFat += saturedFat;
+  }
+
+  console.log(
+    `La suma total de grasas saturadas es de ${totalSumOfSaturedFat}`
+  );
+}
+
+function averagePercentOfEachVitamin() {
+  let vitaminSum = {
+    vitaminA: 0,
+    vitaminC: 0,
+    calcium: 0,
+    iron: 0,
+  };
+
+  for (let i = 0; i < donut.length; i++) {
+    const vitamins = donut[i].nutrition_facts.nutrition.vitamins;
+
+    for (let j = 0; j < vitamins.length; j++) {
+      if (vitamins[j].type.includes("Vitamin A")) {
+        const vitaminA = +vitamins[j].percent.split("%")[0];
+        vitaminSum.vitaminA += vitaminA;
+      } else if (vitamins[j].type.includes("Vitamin C")) {
+        const vitaminC = +vitamins[j].percent.split("%")[0];
+        vitaminSum.vitaminC += vitaminC;
+      } else if (vitamins[j].type.includes("Calcium")) {
+        const calcium = +vitamins[j].percent.split("%")[0];
+        vitaminSum.calcium += calcium;
+      } else {
+        const iron = +vitamins[j].percent.split("%")[0];
+        vitaminSum.iron += iron;
+      }
+    }
+  }
+  let vitaminAverage = {
+    VitaminA: vitaminSum.vitaminA / donut.length,
+    VitaminC: vitaminSum.vitaminC / donut.length,
+    Calcium: vitaminSum.calcium / donut.length,
+    Iron: vitaminSum.iron / donut.length,
+  };
+
+  console.log(`La media de cada vitamina es de:
+    Vitamina A: ${vitaminAverage.VitaminA}%
+    Vitamina C: ${vitaminAverage.VitaminC}%
+    Calcium: ${vitaminAverage.Calcium}%
+    Iron: ${vitaminAverage.Iron}%`);
+}
+
+//3.- El horno a la leña de esta posada es de alta calidad, debemos lanzar un hechizo para saber qué tipo de masa utilizan
+
+//Listar cada donut con sus posibles masas, batter (+ 50 exp)
+
+//Listar cada donut con sus posibles extras topping (+ 50 exp)
+
+//Mostrar el donut con más variedad de masas (batters) (+ 50 exp)
+
+//Mostrar el donut con más variedad de toppings (+ 50 exp)
+
+//Contar el número total de masas y toppings diferentes que existen en toda la posada (+ 50 exp)
+
+//4.- Nuestro grupo sólo dispone de 4 monedas de plata.
+
+//Mostrar cuántos donuts de cada tipo podemos comprar y las monedas sobrantes. (+ 50 exp)
+
+//Encontrar el donut más caro que aún podamos comprar con 4 monedas de plata (+ 50 exp)
+
+//Encontrar el donut más barato de la posada (+ 50 exp)
+
+//Ordenar los donuts por precio de menor a mayor (+ 50 exp)
+
+//Calcular cuántos donuts distintos podríamos comprar con 4 monedas en total (mezclando tipos) (+ 50 exp)
+
+//5.- Para nuestro horror y preocupación hemos detectado grandes errores sintácticos en el conjuro original, es momento de poner nuestros conocimientos arcanos al servicio de toda la posada.
+
+//Los donuts con el colesterol > 12 modificar las grasas trans a 3,2 gr (+ 50 exp)
+
+//Donuts con azúcar > 50  modificar el amount de los detalles de carbohidratos a 42gr (+ 50 exp)
+
+//Añadir una vitamina llamada "Nitacina" al donut con el nombre "Magic Fusion" (+ 50 exp)
+
+//El daily value de los carbohidratos de todos los donuts va a ser de 53% (+ 50 exp)
+
+//Crearle un nuevo atributo "Alergen" al donut llamado "Relaxing Alchemy" y que dentro de el ponga "Gluten Free" (+ 50 exp)
